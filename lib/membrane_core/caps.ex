0cf8b3a4fd4b48cec6985e9faa2ff860d480afd6
Marcin Lewandowski <marcin@radiokit.org>
2016-11-08 23:19:49 +0100

    Improved Caps

--- lib/membrane_core/caps.ex
+++ lib/membrane_core/caps.ex
@@ -1,11 +1,16 @@
 defmodule Membrane.Caps do
   @type content_type :: String.t
   @type channels_type :: non_neg_integer
+  @type sample_rate_type :: non_neg_integer
+  @type frame_size_type :: 1 | 2 | 4
+  @type endianness_type :: :le | :be
 
   @type t :: %Membrane.Caps{
     content: Membrane.Caps.content_type,
     channels: Membrane.Caps.channels_type
+    frame_size: Membrane.Caps.frame_size_type
+    endianness: Membrane.Caps.endianness_type
   }
 
-  defstruct content: nil, channels: nil
+  defstruct content: nil, channels: nil, sample_rate: nil, endianness: nil, frame_size: nil
 end
