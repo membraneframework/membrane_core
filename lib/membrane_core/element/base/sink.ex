cac92fa72ee06de968c723520165f958a783a65d
Marcin Lewandowski <marcin@radiokit.org>
2016-11-10 21:44:35 +0100

    Fixed order of mixin imports in base element behaviours

--- lib/membrane_core/element/base/sink.ex
+++ lib/membrane_core/element/base/sink.ex
@@ -6,6 +6,13 @@ defmodule Membrane.Element.Base.Sink do
 
   defmacro __using__(_) do
     quote location: :keep do
+      # Order here is important
+      #
+      # Calls have to be near each other
+      # Infos have to be near each other
+      #
+      # ...because elixir requires that different pattern matching functions
+      # have to be in a sequence.
       use Membrane.Element.Base.Mixin.CommonFuncs
       use Membrane.Element.Base.Mixin.CommonProcess
       use Membrane.Element.Base.Mixin.CommonCalls
@@ -13,15 +20,10 @@ defmodule Membrane.Element.Base.Sink do
       use Membrane.Element.Base.Mixin.CommonBehaviour
       use Membrane.Element.Base.Mixin.SinkBehaviour
 
-      # Order here is important
-      #
-      # Calls have to be near each other
-      # Infos have to be near each other
-      #
-      # ...because elixir requires that different pattern matching functions
-      # have to be in a seqence.
-      use Membrane.Element.Base.Mixin.CommonInfos
+      # Sink handle_info are more specific than common handle_info
+      # so they should be first
       use Membrane.Element.Base.Mixin.SinkInfos
+      use Membrane.Element.Base.Mixin.CommonInfos
     end
   end
 end
