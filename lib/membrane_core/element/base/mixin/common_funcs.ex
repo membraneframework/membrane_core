f85f7686f516354da67d9cf53896ee8f83e4959b
Marcin Lewandowski <marcin@radiokit.org>
2016-11-15 15:25:55 +0100

    Code cleanup

--- lib/membrane_core/element/base/mixin/common_funcs.ex
+++ lib/membrane_core/element/base/mixin/common_funcs.ex
@@ -9,19 +9,32 @@ defmodule Membrane.Element.Base.Mixin.CommonFuncs do
       use Membrane.Mixins.Log
 
 
+      # Generic handler that can be used to convert return value from
+      # element callback to reply that is accepted by GenServer.handle_info.
+      #
+      # Case when callback returned success and requests no further action.
       defp handle_callback({:ok, new_element_state}, state) do
         debug("Handle callback: OK")
         {:noreply, %{state | element_state: new_element_state}}
       end
 
 
+      # Generic handler that can be used to convert return value from
+      # element callback to reply that is accepted by GenServer.handle_info.
+      #
+      # Case when callback returned success and wants to send some messages
+      # (such as buffers) in response.
       defp handle_callback({:send, message_list, new_element_state}, %{link_destinations: link_destinations} = state) do
         debug("Handle callback: OK + send #{inspect(message_list)}")
-        :ok = send_message_list_loop(message_list, link_destinations)
+        :ok = send_message_list(message_list, link_destinations)
         {:noreply, %{state | element_state: new_element_state}}
       end
 
 
+      # Generic handler that can be used to convert return value from
+      # element callback to reply that is accepted by GenServer.handle_info.
+      #
+      # Case when callback returned failure.
       defp handle_callback({:error, reason, new_element_state}, state) do
         warn("Handle callback: Error (reason = #{inspect(reason)}")
         {:noreply, %{state | element_state: new_element_state}}
@@ -29,29 +42,32 @@ defmodule Membrane.Element.Base.Mixin.CommonFuncs do
       end
 
 
-      # Sends buffer to all linked destinations, final case when list is empty
-      defp send_message_loop(_buffer, []) do
+      # Sends message to all linked destinations, final case when list is empty
+      defp send_message(_message, []) do
         :ok
       end
 
 
-      # Sends buffer to all linked destinations, recurrent case when list is non-empty
-      defp send_message_loop(buffer, [link_destinations_head|link_destinations_tail]) when is_tuple(buffer) do
+      # Sends message to all linked destinations, recurrent case when list
+      # is non-empty and message is a buffer
+      defp send_message(%Membrane.Buffer{} = buffer, [link_destinations_head|link_destinations_tail])  do
         send(link_destinations_head, {:membrane_buffer, buffer})
-        send_message_loop(buffer, link_destinations_tail)
+        send_message(buffer, link_destinations_tail)
       end
 
 
-      # Sends buffer list to all linked destinations, final case when buffer list is empty
-      defp send_message_list_loop([], _link_destinations) do
+      # Sends message list to all linked destinations, final case when message
+      # list is empty
+      defp send_message_list([], _link_destinations) do
         :ok
       end
 
 
-      # Sends buffer list to all linked destinations, recurrent case when buffer list is non-empty
-      defp send_message_list_loop([buffer_head|buffer_tail], link_destinations) when is_tuple(buffer_head) do
-        :ok = send_message_loop(buffer_head, link_destinations)
-        send_message_list_loop(buffer_tail, link_destinations)
+      # Sends message list to all linked destinations, recurrent case when
+      # message list is non-empty
+      defp send_message_list([message_head|message_tail], link_destinations) do
+        :ok = send_message(message_head, link_destinations)
+        send_message_list(message_tail, link_destinations)
       end
     end
   end
