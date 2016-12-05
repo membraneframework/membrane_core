137d559df33d1e5e30e45f42f2f59402aa80ef70
Marcin Lewandowski <marcin@radiokit.org>
2016-12-05 10:26:27 +0100

    Added Helpers.Time.monotonic_time/0, renamed Helpers.Time.ticks_per_sec to Helpers.Time.resolution

--- lib/membrane_core/helpers/time.ex
+++ lib/membrane_core/helpers/time.ex
@@ -5,8 +5,20 @@ defmodule Membrane.Helpers.Time do
   It will return integer that tells how many "ticks" the VM is handling
   within a second.
   """
-  @spec ticks_per_sec() :: non_neg_integer
-  def ticks_per_sec do
+  @spec resolution() :: non_neg_integer
+  def resolution do
     :erlang.convert_time_unit(1, :seconds, :native)
+  end
+
+
+  @doc """
+  Returns current monotonic time.
+
+  It is a wrapper around `:erlang.monotonic_time/0` made mostly for purpose
+  of mocking such calls in specs.
+  """
+  @spec monotonic_time() :: integer
+  def monotonic_time do
+    :erlang.monotonic_time()
   end
 end
