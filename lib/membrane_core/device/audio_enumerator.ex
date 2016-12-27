00bf26d249dcf1af43aff902d9a67f0f444f3b93
Marcin Lewandowski <marcin@radiokit.org>
2016-12-27 18:46:55 +0100

    Fixed Membrane.Device.AudioEnumerator behaviour

--- lib/membrane_core/device/audio_enumerator.ex
+++ lib/membrane_core/device/audio_enumerator.ex
@@ -9,6 +9,14 @@ defmodule Membrane.Device.AudioEnumerator do
   @doc """
   Returns list of available audio devices.
   """
-  @callback list() :: {:ok, [] | [%AudioDevice{}]} | {:error, reason}
-  
+  @callback list(:capture | :playback | :all) ::
+    {:ok, [] | [%AudioDevice{}]} |
+    {:error, reason}
+
+
+  defmacro __using__(_) do
+    quote location: :keep do
+      @behaviour Membrane.Device.AudioEnumerator
+    end
+  end
 end
