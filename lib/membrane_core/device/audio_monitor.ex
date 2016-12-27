38a31e9d23a8c450d1e49d7c4e8e2790a740ee69
Marcin Lewandowski <marcin@radiokit.org>
2016-12-27 20:07:17 +0100

    Fixed pattern matching when retreiving devices in Membrane.Device.AudioMonitor

--- lib/membrane_core/device/audio_monitor.ex
+++ lib/membrane_core/device/audio_monitor.ex
@@ -117,9 +117,7 @@ defmodule Membrane.Device.AudioMonitor do
 
 
   def connect(:init, %{module: module, enumerators: enumerators, interval: interval, internal_state: internal_state} = state) do
-    {devices, _, _} = list_devices(enumerators, [])
-
-    case devices do
+    case list_devices(enumerators, []) do
       [] ->
         {:backoff, interval, %{state | previous_devices: devices}}
 
