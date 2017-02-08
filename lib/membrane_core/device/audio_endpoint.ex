cc10499d0395fa9c49457eb9cdd16329e2fe9331
Marcin Lewandowski <marcin@radiokit.org>
2017-02-08 14:53:09 +0100

    Added state to AudioEndpoint

--- lib/membrane_core/device/audio_endpoint.ex
+++ lib/membrane_core/device/audio_endpoint.ex
@@ -1,10 +1,11 @@
 defmodule Membrane.Device.AudioEndpoint do
   @moduledoc """
-  Structure representing a single audio device.
+  Structure representing a single audio endpoint.
 
   It is meant to provide uniform and extendable interface for storing lists
-  of devices, e.g. returned by enumerators that are supposed to determine
-  what hardware is available in the system.
+  of endpoints, e.g. returned by enumerators that are supposed to determine
+  what hardware is available in the system, hiding underlying differences
+  between audio drivers.
 
   It contains the following fields and all of them should be present:
 
@@ -14,7 +15,9 @@ defmodule Membrane.Device.AudioEndpoint do
   * `id` - unique identifier of the device, that is persistent accross reboots,
   * `driver` - atom identyfing driver used for accessing the device, e.g. `:wasapi`,
     `:alsa`,
-  * `direction` - an be either `:capture` or `:playback`.
+  * `direction` - an be either `:capture` or `:playback`,
+  * `state` - endpoint state, can be one of `:active`, `:disabled`, `:notpresent`
+    or `:unplugged`.
   """
 
   @type id_t        :: String.t
@@ -22,6 +25,7 @@ defmodule Membrane.Device.AudioEndpoint do
   @type interface_t :: String.t | nil
   @type driver_t    :: atom
   @type direction_t :: :capture | :playback
+  @type state_t     :: :active | :disabled | :notpresent | :unplugged
 
   @type t :: %Membrane.Device.AudioEndpoint{
     name: name_t,
@@ -29,6 +33,7 @@ defmodule Membrane.Device.AudioEndpoint do
     driver: driver_t,
     direction: direction_t,
     interface: interface_t,
+    state: state_t,
   }
 
   defstruct \
@@ -36,5 +41,6 @@ defmodule Membrane.Device.AudioEndpoint do
     id: nil,
     driver: nil,
     direction: nil,
-    interface: nil
+    interface: nil,
+    state: nil
 end
