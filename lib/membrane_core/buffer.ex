2fb2fd63860641c48da48f2989e7d891f1f4b182
Marcin Lewandowskig <marcin@radiokit.org>
2017-02-20 15:15:35 +0100

    Removed caps field from Membrane.Buffer struct

--- lib/membrane_core/buffer.ex
+++ lib/membrane_core/buffer.ex
@@ -2,26 +2,20 @@ defmodule Membrane.Buffer do
   @moduledoc """
   Structure representing a single chunk of data that flows between elements.
 
-  It is just a wrapper around bitstring so there can be some metadata attached
-  to it.
+  It is just a wrapper around bitstring. In the future they might be some
+  metadata attached to it.
 
   Each buffer:
 
-  - must contain payload,
-  - may contain caps describing format of the payload.
-
-  If caps are nil, it means that payload has unknown format.
+  - must contain payload.
   """
 
   @type payload_t :: bitstring
-  @type caps_t    :: map
 
   @type t :: %Membrane.Buffer{
-    caps: caps_t,
     payload: payload_t,
   }
 
   defstruct \
-    caps: nil,
     payload: nil
 end
