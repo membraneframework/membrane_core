1f1a3f4ba92457a93e88e469d796d8b41625ce73
Mateusz Front <mateusz.front@swmansion.com>
2017-06-23 16:11:01 +0200

    fixes in PullBuffer

--- lib/membrane_core/pull_buffer.ex
+++ lib/membrane_core/pull_buffer.ex
@@ -24,7 +24,7 @@ defmodule Membrane.PullBuffer do
     }
   end
 
-  def fill(%PullBuffer{demand: demand} = pb), do: handle_demand(pb, demand)
+  def fill(%PullBuffer{} = pb), do: handle_demand(pb, 0)
     |> orWarnError("Unable to fill PullBuffer: #{inspect pb}")
 
   def store(%PullBuffer{q: q, current_size: size, preferred_size: pref_size, sink_name: sink} = pb, v) do
@@ -35,9 +35,12 @@ defmodule Membrane.PullBuffer do
       undemanded buffers is controlled and limited.
 
       Buffers: #{inspect v}
+
+      PullBuffer #{inspect pb}
       """
     end
 
+    report "Storing #{inspect (if is_list v do length v else 1 end)} buffers", pb
     if is_list v do
       %PullBuffer{pb | q: q |> @qe.join(@qe.new v), current_size: size + length v}
     else
@@ -45,60 +48,69 @@ defmodule Membrane.PullBuffer do
     end
   end
 
-  def take(%PullBuffer{} = pb) do
-    {out, pb} = do_take pb
-    packOutWithPb out, pb |> handle_demand(1)
+  def take(%PullBuffer{current_size: size} = pb, count \\ 1) when count >= 0 do
+    report "Taking #{inspect count} buffers", pb
+    {out, %PullBuffer{current_size: new_size} = pb} = do_take pb, count
+    with {:ok, pb} <- pb |> handle_demand(size - new_size)
+    do {:ok, {out, pb}}
+    end
   end
-  def take(%PullBuffer{current_size: size} = pb, count) when count >= 0 do
-    {out, %PullBuffer{current_size: new_size} = pb} = do_take_many pb, count
-    packOutWithPb out, pb |> handle_demand(size - new_size)
-  end
+
+  defp do_take(pb, 1), do: do_take(pb)
+  defp do_take(pb, count), do: do_take(pb, count, [])
 
   defp do_take(%PullBuffer{q: q, init_size: init_size, current_size: size} = pb)
   when size > init_size do
-    case q |> @qe.pop do
-      {o, q} -> {o, %PullBuffer{pb | q: q, current_size: max(0, size - 1)}}
-    end
+    {o, q} = q |> @qe.pop
+    if o == :empty do report "underrun", pb end
+    {o, %PullBuffer{pb | q: q, current_size: max(0, size - 1)}}
   end
   defp do_take(%PullBuffer{current_size: size, init_size: init_size} = pb)
   when size < init_size do
+    report """
+      Forbidden to take buffers, as PullBuffer did not reach initial size
+      of #{inspect init_size}, returning :empty
+      """, pb
     {:empty, pb}
   end
-  defp do_take(%PullBuffer{} = pb) do
-    do_take %PullBuffer{pb | init_size: -1}
-  end
+  defp do_take(%PullBuffer{} = pb), do: do_take(%PullBuffer{pb | init_size: -1})
 
-  defp do_take_many(pb, count, acc \\ [])
-  defp do_take_many(%PullBuffer{} = pb, 0, acc), do: {{:value, acc |> Enum.reverse}, pb}
-  defp do_take_many(%PullBuffer{} = pb, count, acc) do
+  defp do_take(%PullBuffer{} = pb, 0, acc), do: {{:value, acc |> Enum.reverse}, pb}
+  defp do_take(%PullBuffer{} = pb, count, acc) do
     case do_take pb do
-      {{:value, v}, npb} -> do_take_many npb, count-1, [v|acc]
+      {{:value, v}, npb} -> do_take npb, count-1, [v|acc]
       {:empty, npb} -> {{:empty, acc |> Enum.reverse}, npb}
     end
   end
 
-  defp packOutWithPb(out, {:ok, pb}), do: {:ok, {out, pb}}
-  defp packOutWithPb(_out, {:error, reason}), do: {:error, reason}
-
-  def empty?(%PullBuffer{current_size: size}), do: size == 0
+  def empty?(%PullBuffer{current_size: size, init_size: init_size}), do:
+    size == 0 || size < init_size
 
   defp handle_demand(%PullBuffer{sink: sink, sink_name: sink_name,
     current_size: size, preferred_size: pref_size, demand: demand} = pb, new_demand)
   when size < pref_size and demand + new_demand > 0 do
-    debug """
-      PullBuffer: sending demand of size #{inspect demand + new_demand}
+    report """
+      Sending demand of size #{inspect demand + new_demand}
       to sink #{inspect sink_name}
-      """
+      """, pb
     with :ok <- GenServer.call(sink, {:membrane_demand, demand + new_demand})
     do {:ok, %PullBuffer{pb | demand: 0}}
     else {:error, reason} -> warnError """
       PullBuffer: unable to send demand of size #{inspect demand + new_demand}
       to sink #{inspect sink_name}
+
+      PullBuffer #{inspect pb}
       """, reason
     end
 
   end
   defp handle_demand(%PullBuffer{demand: demand} = pb, new_demand), do:
     {:ok, %PullBuffer{pb | demand: demand + new_demand}}
+
+  defp report(msg, %PullBuffer{current_size: size, preferred_size: pref_size}),
+  do: inspect """
+    PullBuffer: #{inspect msg}
+    PullBuffer size: #{inspect size}, PullBuffer preferred size: #{inspect pref_size}
+    """
 
 end
