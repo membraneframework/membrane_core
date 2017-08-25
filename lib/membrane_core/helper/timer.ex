def90b5ed079e5bdb70960597d3d8ab134a04a94
Mateusz Front <mateusz.front@swmansion.com>
2017-08-25 15:35:50 +0200

    changed send_at to send_after

--- lib/membrane_core/helper/timer.ex
+++ lib/membrane_core/helper/timer.ex
@@ -2,11 +2,14 @@ defmodule Membrane.Helper.Timer do
   use Membrane.Helper
   require Membrane.Time, as: Time
 
-  def send_at(time, pid \\ self(), msg) when Time.is_t(time) do
+  def send_after(time, min_delay \\ nil, pid \\ self(), msg)
+  when Time.is_t(time) and (is_nil(min_delay) or Time.is_t(min_delay)) and is_pid(pid)
+  do
     time
-        |> Kernel.-(Time.system_time)
-        |> Time.to_milliseconds
-        |> :timer.send_after(pid, msg)
+      |> Kernel.-(Time.system_time)
+      ~> (t -> if is_nil(min_delay) do t else max t, min_delay end)
+      |> Time.to_milliseconds
+      |> :timer.send_after(pid, msg)
   end
 
 end
