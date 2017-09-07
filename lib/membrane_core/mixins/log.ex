d167b74856be1f73553acb947397e53acdf051b1
Mateusz Front <mateusz.front@swmansion.com>
2017-09-07 13:39:47 +0200

    WIP: log module refactor

--- lib/membrane_core/mixins/log.ex
+++ lib/membrane_core/mixins/log.ex
@@ -5,76 +5,86 @@ defmodule Membrane.Mixins.Log do
   """
 
   use Membrane.Helper
+  alias Membrane.Log.Router
 
   @doc false
   defmacro __using__(args) do
     default_tags = args |> Keyword.get(:tags, []) |> Helper.listify
+    Module.register_attribute __CALLER__.module, :logger_default_tags, accumulate: true
+    Module.put_attribute __CALLER__.module, :logger_default_tags, default_tags
+
+    if args |> Keyword.get(:import, true) do
+      quote location: :keep do
+        import Membrane.Mixins.Log
+      end
+    end
+
+  end
+
+  @doc false
+  defmacro debug(message, tags \\ []) do
     quote location: :keep do
-      alias Membrane.Log.Router
+      log :debug, unquote(message), unquote(tags)
+    end
+  end
 
-      @doc false
-      defmacrop log(level, message, tags) do
-        config = Application.get_env(:membrane_core, Membrane.Logger, [])
-        router_level = config |> Keyword.get(:level, :debug)
-        router_level_val = router_level |> Router.level_to_val
+  @doc false
+  defmacro info(message, tags \\ []) do
+    quote location: :keep do
+      log :info, unquote(message), unquote(tags)
+    end
+  end
 
-        quote location: :keep do
-          level_val = unquote(level) |> Router.level_to_val
-          if level_val >= unquote(router_level_val) do
-            Router.send_log(unquote(level),  unquote(message), Membrane.Time.monotonic_time, unquote(tags))
-          end
-        end
-      end
+  @doc false
+  defmacro warn(message, tags \\ []) do
+    quote location: :keep do
+      log :warn, unquote(message), unquote(tags)
+    end
+  end
 
-      @doc false
-      defmacrop info(message, tags \\ []) do
-        default_tags = unquote default_tags
-        quote location: :keep do
-          log(:info, unquote(message), unquote(tags) ++ unquote(default_tags))
-        end
-      end
+  defmacro warn_error(message, reason, tags \\ []) do
+    quote do
+      use Membrane.Helper
+      warn """
+      Encountered an error:
+      #{unquote message}
+      Reason: #{inspect unquote reason}
+      Stacktrace:
+      #{Helper.stacktrace}
+      """, unquote(tags)
+      unquote {:error, reason}
+    end
+  end
 
-      @doc false
-      defmacrop warn(message, tags \\ []) do
-        default_tags = unquote default_tags
-        quote location: :keep do
-          log(:warn, unquote(message), unquote(tags) ++ unquote(default_tags))
-        end
-      end
-
-      defmacrop warn_error(message, reason) do
-        quote do
-          use Membrane.Helper
-          warn """
-          Encountered an error:
-          #{unquote message}
-          Reason: #{inspect unquote reason}
-          Stacktrace:
-          #{Helper.stacktrace}
-          """
-          unquote {:error, reason}
-        end
-      end
-
-      defmacrop or_warn_error(v, message) do
-        quote do
-          with {:ok, value} <- unquote v
-          do {:ok, value}
-          else {:error, reason} -> warn_error(unquote(message), reason)
-          end
-        end
-      end
-      # defp or_warn_error({:ok, value}, _msg), do: {:ok, value}
-      # defp or_warn_error({:error, reason}, msg), do: warn_error(msg, reason)
-
-
-      @doc false
-      defmacrop debug(message, tags \\ []) do
-        default_tags = unquote default_tags
-        quote location: :keep do
-          log(:debug, unquote(message), unquote(tags) ++ unquote(default_tags))
-        end
+  defmacro or_warn_error(v, message, tags \\ []) do
+    quote do
+      with {:ok, value} <- unquote v
+      do {:ok, value}
+      else {:error, reason} -> warn_error unquote(message), reason, unquote(tags)
       end
     end
   end
+
+
+  @doc false
+  defmacro log(level, message, tags) do
+    config = Application.get_env :membrane_core, Membrane.Logger, []
+    router_level = config |> Keyword.get(:level, :debug)
+    router_level_val = router_level |> Router.level_to_val
+
+    quote location: :keep do
+      alias Membrane.Log.Router
+      use Membrane.Helper
+      level_val = unquote(level) |> Router.level_to_val
+      if level_val >= unquote(router_level_val) do
+        Router.send_log(
+          unquote(level),
+          unquote(message),
+          Membrane.Time.monotonic_time,
+          (unquote(tags) |> Helper.listify) ++ (@logger_default_tags |> List.flatten)
+        )
+      end
+    end
+  end
+
 end
