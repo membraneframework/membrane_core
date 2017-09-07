910da67a1d0b72f197fc42416bbf7184f53b2b4f
Mateusz Front <mateusz.front@swmansion.com>
2017-09-07 14:23:43 +0200

    logger refactor, now can use within functions and define module-wide default tags

--- lib/membrane_core/mixins/log.ex
+++ lib/membrane_core/mixins/log.ex
@@ -4,49 +4,61 @@ defmodule Membrane.Mixins.Log do
   modules.
   """
 
+  alias Membrane.Log.Router
+
   @doc false
   defmacro __using__(args) do
     default_tags = args |> Keyword.get(:tags, [])
+    Module.register_attribute __CALLER__.module, :logger_default_tags, accumulate: true
+    Module.put_attribute __CALLER__.module, :logger_default_tags, default_tags
+
+    if args |> Keyword.get(:import, true) do
+      quote location: :keep do
+        import Membrane.Mixins.Log
+      end
+    end
+
+  end
+
+  @doc false
+  defmacro debug(message, tags \\ []) do
+    quote location: :keep do
+      log :debug, unquote(message), unquote(tags)
+    end
+  end
+
+  @doc false
+  defmacro info(message, tags \\ []) do
+    quote location: :keep do
+      log :info, unquote(message), unquote(tags)
+    end
+  end
+
+  @doc false
+  defmacro warn(message, tags \\ []) do
+    quote location: :keep do
+      log :warn, unquote(message), unquote(tags)
+    end
+  end
+
+  @doc false
+  defmacro log(level, message, tags) do
+    config = Application.get_env :membrane_core, Membrane.Logger, []
+    router_level = config |> Keyword.get(:level, :debug)
+    router_level_val = router_level |> Router.level_to_val
+
     quote location: :keep do
       alias Membrane.Log.Router
-
-      @doc false
-      defmacro log(level, message, tags) do
-        config = Application.get_env(:membrane_core, Membrane.Logger, [])
-        router_level = config |> Keyword.get(:level, :debug)
-        router_level_val = router_level |> Router.level_to_val
-
-        quote location: :keep do
-          level_val = unquote(level) |> Router.level_to_val
-          if level_val >= unquote(router_level_val) do
-            Router.send_log(unquote(level),  unquote(message), Membrane.Time.native_monotonic_time, unquote(tags))
-          end
-        end
-      end
-
-      @doc false
-      defmacro info(message, tags \\ []) do
-        default_tags = unquote default_tags
-        quote location: :keep do
-          log(:info, unquote(message), unquote(tags) ++ unquote(default_tags))
-        end
-      end
-
-      @doc false
-      defmacro warn(message, tags \\ []) do
-        default_tags = unquote default_tags
-        quote location: :keep do
-          log(:warn, unquote(message), unquote(tags) ++ unquote(default_tags))
-        end
-      end
-
-      @doc false
-      defmacro debug(message, tags \\ []) do
-        default_tags = unquote default_tags
-        quote location: :keep do
-          log(:debug, unquote(message), unquote(tags) ++ unquote(default_tags))
-        end
+      level_val = unquote(level) |> Router.level_to_val
+      if level_val >= unquote(router_level_val) do
+        Router.send_log(
+          unquote(level),
+          unquote(message),
+          Membrane.Time.native_monotonic_time,
+          ([unquote(tags)] |> List.flatten) ++ (@logger_default_tags |> List.flatten)
+        )
       end
     end
   end
+
 end
