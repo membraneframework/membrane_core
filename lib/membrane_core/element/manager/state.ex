23bb73ac50d365ffa399baf6d52684dcb404bfb4
Mateusz Nowak <mateusz.nowak@swmansion.com>
2018-05-16 10:43:56 +0200

    Fix requested in review

--- lib/membrane_core/element/manager/state.ex
+++ lib/membrane_core/element/manager/state.ex
@@ -79,45 +79,39 @@ defmodule Membrane.Element.Manager.State do
   end
 
   def link_pad(state, {:dynamic, name, _no} = full_name, direction, init_f) do
-    with {:ok, %{direction: ^direction} = data} <-
-           state.pads.info[name]
-           |> Helper.wrap_nil(:unknown_pad)
-           ~>> (%{is_dynamic: false} -> {:error, :not_dynamic_pad}) do
+    with %{direction: ^direction, is_dynamic: true} = data <- state.pads.info[name] do
       {:ok, state} = state |> init_pad_data(full_name, data, init_f)
       state = state |> add_to_currently_linking(full_name)
       {:ok, state}
     else
-      {:ok, %{direction: actual_direction}} ->
+      %{direction: actual_direction} ->
         {:error, {:invalid_pad_direction, [expected: direction, actual: actual_direction]}}
 
-      error ->
-        error
+      %{is_dynamic: false} ->
+        {:error, :not_dynamic_pad}
+
+      nil ->
+        {:error, :unknown_pad}
     end
   end
 
   def link_pad(state, name, direction, init_f) do
-    with {:ok, %{direction: ^direction} = data} <-
-           state.pads.info[name]
-           |> Helper.wrap_nil(:unknown_pad)
-           ~>> (%{is_dynamic: true} -> {:error, :not_static_pad}) do
-      {:ok, state} =
-        state
-        |> Helper.Struct.update_in([:pads, :info], &(&1 |> Map.delete(name)))
-        |> init_pad_data(name, data, init_f)
-
-      {:ok, state}
+    with %{direction: ^direction, is_dynamic: false} = data <- state.pads.info[name] do
+      state
+      |> Helper.Struct.update_in([:pads, :info], &(&1 |> Map.delete(name)))
+      |> init_pad_data(name, data, init_f)
     else
-      {:ok, %{direction: actual_direction}} ->
+      %{direction: actual_direction} ->
         {:error, {:invalid_pad_direction, [expected: direction, actual: actual_direction]}}
 
-      {:error, :unknown_pad} ->
+      %{is_dynamic: true} ->
+        {:error, :not_static_pad}
+
+      nil ->
         case get_pad_data(state, name, direction) do
           {:ok, _} -> {:error, :already_linked}
           _ -> {:error, :unknown_pad}
         end
-
-      error ->
-        error
     end
   end
 
