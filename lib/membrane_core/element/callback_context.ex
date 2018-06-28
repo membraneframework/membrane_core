9dfe221a5396f24389bbed1d80bdebffdaf2213a
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2018-06-28 15:55:43 +0200

    Add miss specs and docs

--- lib/membrane_core/element/callback_context.ex
+++ lib/membrane_core/element/callback_context.ex
@@ -1,4 +1,8 @@
 defmodule Membrane.Element.CallbackContext do
+  @moduledoc """
+  Parent module for all contexts passed to callbacks. Provides a convenient way
+  to create them
+  """
   alias Membrane.Element.CallbackContext, as: Ctx
 
   @callback from_state(Membrane.Element.Manager.State.t(), keyword()) :: struct()
@@ -17,6 +21,27 @@ defmodule Membrane.Element.CallbackContext do
     :handle_write => Ctx.Write
   }
 
+  @type callback_t() ::
+          :handle_prepare
+          | :handle_play
+          | :handle_caps
+          | :handle_demand
+          | :handle_event
+          | :handle_other
+          | :handle_pad_added
+          | :handle_pad_removed
+          | :handle_process
+          | :handle_stop
+          | :handle_write
+
+  @doc """
+  Provides a proper Context struct for a callback.
+  """
+  @spec construct!(
+          context :: callback_t(),
+          state :: Membrane.Element.Manager.State.t(),
+          entries :: keyword()
+        ) :: struct()
   def construct!(callback, state, entries \\ []) do
     ctx_module = @context_mapping |> Map.get(callback)
 
