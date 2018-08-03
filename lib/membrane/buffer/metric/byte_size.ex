ed63b3877bc528dd40c623604013d2d11734339f
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2018-08-03 16:31:15 +0200

    Avoid wrapping binary payload in a struct

--- lib/membrane/buffer/metric/byte_size.ex
+++ lib/membrane/buffer/metric/byte_size.ex
@@ -14,19 +14,18 @@ defmodule Membrane.Buffer.Metric.ByteSize do
 
   def split_buffers(buffers, count), do: do_split_buffers(buffers, count, [])
 
-  defp do_split_buffers([%Buffer{payload: p} = buf | rest], at_pos, acc) do
-    cond do
-      at_pos == 0 or rest == [] ->
-        {acc |> Enum.reverse(), rest}
+  defp do_split_buffers(buffers, at_pos, acc) when at_pos == 0 or buffers == [] do
+    {acc |> Enum.reverse(), buffers}
+  end
 
-      at_pos < Payload.size(p) and at_pos > 0 ->
-        {p1, p2} = Payload.split_at(p, at_pos)
-        acc = [%Buffer{buf | payload: p1} | acc] |> Enum.reverse()
-        rest = [%Buffer{buf | payload: p2} | rest]
-        {acc, rest}
-
-      at_pos >= Payload.size(p) ->
-        do_split_buffers(rest, at_pos - Payload.size(p), [buf | acc])
+  defp do_split_buffers([%Buffer{payload: p} = buf | rest], at_pos, acc) when at_pos > 0 do
+    if at_pos < Payload.size(p) do
+      {p1, p2} = Payload.split_at(p, at_pos)
+      acc = [%Buffer{buf | payload: p1} | acc] |> Enum.reverse()
+      rest = [%Buffer{buf | payload: p2} | rest]
+      {acc, rest}
+    else
+      do_split_buffers(rest, at_pos - Payload.size(p), [buf | acc])
     end
   end
 end
--- lib/membrane/payload.ex
+++ lib/membrane/payload.ex
@@ -1,15 +1,50 @@
 defprotocol Membrane.Payload do
+  @moduledoc """
+  """
+
   @type t :: struct()
 
-  @doc "Returns total size of paload in bytes"
+  @doc """
+  Returns total size of paload in bytes
+  """
   @spec size(payload :: t()) :: non_neg_integer()
   def size(payload)
 
-  @doc "Splits the payload at given position (1st part has the size equal to `at_pos` argument)"
+  @doc """
+  Splits the payload at given position (1st part has the size equal to `at_pos` argument)
+  """
   @spec split_at(payload :: t(), at_pos :: non_neg_integer()) :: {t(), t()}
   def split_at(payload, at_pos)
 
-  @doc "Converts payload into binary"
+  @doc """
+  Converts payload into binary
+  """
   @spec to_binary(t()) :: binary()
   def to_binary(payload)
+end
+
+defimpl Membrane.Payload, for: BitString do
+  @spec size(payload :: binary()) :: non_neg_integer
+  def size(data) when is_binary(data) do
+    data |> byte_size()
+  end
+
+  @spec split_at(binary(), non_neg_integer) :: {binary(), binary()}
+  def split_at(data, 0) do
+    {<<>>, data}
+  end
+
+  def split_at(data, at_pos) when byte_size(data) <= at_pos do
+    {data, <<>>}
+  end
+
+  def split_at(data, at_pos) do
+    <<part1::binary-size(at_pos), part2::binary>> = data
+    {part1, part2}
+  end
+
+  @spec to_binary(binary()) :: binary()
+  def to_binary(data) when is_binary(data) do
+    data
+  end
 end
