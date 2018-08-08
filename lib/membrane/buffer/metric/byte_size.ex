78d5a50ebb61c608be964e078e3fc62e55246924
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2018-08-08 08:52:11 +0200

    Don't allow empty payload as split_at result

--- lib/membrane/buffer/metric/byte_size.ex
+++ lib/membrane/buffer/metric/byte_size.ex
@@ -20,7 +20,7 @@ defmodule Membrane.Buffer.Metric.ByteSize do
 
   defp do_split_buffers([%Buffer{payload: p} = buf | rest], at_pos, acc) when at_pos > 0 do
     if at_pos < Payload.size(p) do
-      {p1, p2} = Payload.split_at(p, at_pos)
+      {p1, p2} = Payload.split_at!(p, at_pos)
       acc = [%Buffer{buf | payload: p1} | acc] |> Enum.reverse()
       rest = [%Buffer{buf | payload: p2} | rest]
       {acc, rest}
--- lib/membrane/payload.ex
+++ lib/membrane/payload.ex
@@ -6,7 +6,7 @@ defprotocol Membrane.Payload do
   is implemented by the Membrane Core.
   """
 
-  @type t :: struct()
+  @type t :: any()
 
   @doc """
   Returns total size of payload in bytes
@@ -16,33 +16,36 @@ defprotocol Membrane.Payload do
 
   @doc """
   Splits the payload at given position (1st part has the size equal to `at_pos` argument)
+
+  `at_pos` has to be greater than 0 and smaller than the size of payload. This guarantees
+  returned payloads are never empty.
+
+  When such conditions are not met, the function should raise.
   """
-  @spec split_at(payload :: t(), at_pos :: non_neg_integer()) :: {t(), t()}
-  def split_at(payload, at_pos)
+  @spec split_at!(payload :: t(), at_pos :: pos_integer()) :: {t(), t()}
+  def split_at!(payload, at_pos)
 
   @doc """
   Converts payload into binary
   """
   @spec to_binary(t()) :: binary()
   def to_binary(payload)
+
+  @doc """
+  Returns an atom describing type of the payload.
+  """
+  @spec type(t()) :: atom()
+  def type(payload)
 end
 
 defimpl Membrane.Payload, for: BitString do
-  @spec size(payload :: binary()) :: non_neg_integer
+  @spec size(payload :: binary()) :: pos_integer
   def size(data) when is_binary(data) do
     data |> byte_size()
   end
 
-  @spec split_at(binary(), non_neg_integer) :: {binary(), binary()}
-  def split_at(data, 0) do
-    {<<>>, data}
-  end
-
-  def split_at(data, at_pos) when byte_size(data) <= at_pos do
-    {data, <<>>}
-  end
-
-  def split_at(data, at_pos) do
+  @spec split_at!(binary(), pos_integer) :: {binary(), binary()}
+  def split_at!(data, at_pos) when 0 < at_pos and at_pos < byte_size(data) do
     <<part1::binary-size(at_pos), part2::binary>> = data
     {part1, part2}
   end
@@ -51,4 +54,7 @@ defimpl Membrane.Payload, for: BitString do
   def to_binary(data) when is_binary(data) do
     data
   end
+
+  @spec type(binary()) :: :binary
+  def type(_), do: :binary
 end
