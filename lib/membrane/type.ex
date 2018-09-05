867ad7976bae4dcef2914e219e603923159a965f
Mateusz Front <mateusz.front@swmansion.com>
2018-09-05 14:35:35 +0200

    extracted Membrane.Type module to Bunch

--- lib/membrane/type.ex
+++
@@ -1,9 +0,0 @@
-defmodule Membrane.Type do
-  @type try_t :: :ok | {:error, reason :: any}
-  @type try_t(value) :: {:ok, value} | {:error, reason :: any}
-
-  @type stateful_t(value, state) :: {value, state}
-
-  @type stateful_try_t(state) :: stateful_t(try_t, state)
-  @type stateful_try_t(value, state) :: stateful_t(try_t(value), state)
-end
