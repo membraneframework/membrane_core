a1444dafe97262b22d1e7a3c0abfbc6a5821a98b
Vladyslav Hlembotskyi <nobikik9@gmail.com>
2018-09-07 15:21:11 +0200

    Replaced the function child_name? with a guard

--- lib/membrane_core/pipeline.ex
+++ lib/membrane_core/pipeline.ex
@@ -16,6 +16,7 @@ defmodule Membrane.Pipeline do
   alias Membrane.{Element, Message}
   use Membrane.Helper
   import Helper.GenServer
+  require Membrane.Element
 
   @typedoc """
   Defines options that can be passed to `start/3` / `start_link/3` and received
@@ -278,35 +279,18 @@ defmodule Membrane.Pipeline do
     end
   end
 
-  defp child_name?(name) when is_atom(name), do: true
-
-  defp child_name?({name_atom, name_id})
-       when is_atom(name_atom) and is_integer(name_id) and name_id >= 0,
-       do: true
-
-  defp child_name?(_name), do: false
-
   defp parse_children(children) when is_map(children) or is_list(children),
     do: children |> Helper.Enum.map_with(&parse_child/1)
 
-  defp parse_child({name, {%module{} = options, params}} = config)
-       when is_list(params) do
-    if child_name?(name) do
-      {:ok, %{name: name, module: module, options: options, params: params}}
-    else
-      {:error, invalid_child_config: config}
-    end
+  defp parse_child({name, {%module{} = options, params}})
+       when Element.is_element_name(name) and is_list(params) do
+    {:ok, %{name: name, module: module, options: options, params: params}}
   end
 
-  defp parse_child({name, {module, params}} = config)
-       when is_atom(module) and is_list(params) do
+  defp parse_child({name, {module, params}})
+       when Element.is_element_name(name) and is_atom(module) and is_list(params) do
     options = module |> Helper.Module.struct()
-
-    if child_name?(name) do
-      {:ok, %{name: name, module: module, options: options, params: params}}
-    else
-      {:error, invalid_child_config: config}
-    end
+    {:ok, %{name: name, module: module, options: options, params: params}}
   end
 
   defp parse_child({name, module}) do
