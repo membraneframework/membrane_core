27449e99183aa4d516917fa6227a13e08f2c3ed2
Mateusz Front <mateusz.front@swmansion.com>
2018-09-18 18:25:18 +0200

    bumped required elixir version to 1.7

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Membrane.Mixfile do
     [
       app: :membrane_core,
       version: @version,
-      elixir: "~> 1.6",
+      elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       description: "Membrane Multimedia Framework (Core)",
       package: package(),
