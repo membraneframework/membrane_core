ed66f47b2e7a06cda80c4122db54df04871c627f
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2018-11-20 14:15:41 +0100

    Link to types from def_*_pad

--- lib/membrane/element/base/mixin/sink_behaviour.ex
+++ lib/membrane/element/base/mixin/sink_behaviour.ex
@@ -27,7 +27,7 @@ defmodule Membrane.Element.Base.Mixin.SinkBehaviour do
             ) :: CommonBehaviour.callback_return_t()
 
   @doc """
-  Macro that defines known input pads for the element type.
+  Macro that defines input pads for the element type.
 
   Allows to use `Membrane.Caps.Matcher.one_of/1` and `Membrane.Caps.Matcher.range/2`
   functions without module prefix.
@@ -35,13 +35,7 @@ defmodule Membrane.Element.Base.Mixin.SinkBehaviour do
   It automatically generates documentation from the given definition
   and adds compile-time caps specs validation.
 
-  The definition of pads should be a keyword list with pad name as key and
-  another keyword list as a value. The inner list may contain the following entries:
-  * `availability:` `t:Membrane.Element.Pad.availability_t/0` - defaults to `:always`,
-  * `mode:` `t:Membrane.Element.Pad.mode_t/0`, defaults to `:pull`,
-  * `demand_unit:` `t:Membrane.Buffer.Metric.unit_t/0`, required if mode is set to `pull`,
-  * `caps:` `t:Membrane.Caps.Matcher.caps_specs_t/0` - always required, should contain the specification
-    of allowed caps. See `Membrane.Caps.Matcher` for details on how to define it.
+  The type `t:Membrane.Element.Pad.input_spec_t/0` describes how the definition of pads should look.
   """
   defmacro def_input_pads(pads) do
     PadsSpecsParser.def_pads(pads, :input)
