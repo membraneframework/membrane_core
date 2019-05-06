abac006655df873cbf1d4b116a756aeaf9ffdb2a
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2019-05-06 13:39:22 +0200

    Make trivial elemets really trivial

--- spec/support/element/trivial_sink.ex
+++ spec/support/element/trivial_sink.ex
@@ -1,43 +1,9 @@
 defmodule Membrane.Support.Element.TrivialSink do
   @moduledoc """
-  This is minimal sample sink element for use in specs.
-
-  Modify with caution as many specs may depend on its shape.
+  This is the most basic sink. It does nothing, but is used in specs.
   """
 
   use Membrane.Element.Base.Sink
 
   def_input_pad :input, caps: :any, demand_unit: :buffers
-
-  @impl true
-  def handle_init(_options) do
-    {:ok, %{timer: nil}}
-  end
-
-  @impl true
-  def handle_stopped_to_prepared(%Ctx.PlaybackChange{}, state), do: {:ok, state}
-
-  def handle_playing_to_prepared(%Ctx.PlaybackChange{}, %{timer: timer}) do
-    if timer do
-      :timer.cancel(timer)
-    end
-
-    {:ok, %{timer: nil}}
-  end
-
-  @impl true
-  def handle_prepared_to_playing(%Ctx.PlaybackChange{}, state) do
-    {:ok, timer} = :timer.send_interval(500, :tick)
-    {:ok, %{state | timer: timer}}
-  end
-
-  @impl true
-  def handle_other(:tick, %Ctx.Other{}, state) do
-    {{:ok, demand: {:input, 2}}, state}
-  end
-
-  @impl true
-  def handle_write(:input, _buf, %Ctx.Write{}, state) do
-    {:ok, state}
-  end
 end
