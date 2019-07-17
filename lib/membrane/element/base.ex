75c862426fa81aa149c6fe51504d352c23806128
Dominik Stanaszek <dominik.stanaszek@swmansion.com>
2019-07-17 09:17:16 +0200

    Merge moduledocs of Element.Base and CommonBehaviour

--- lib/membrane/element/base.ex
+++ lib/membrane/element/base.ex
@@ -5,8 +5,46 @@ defmodule Membrane.Element.Base do
   When used declares behaviour implementation, provides default callback definitions
   and imports macros.
 
-  For more information on implementing elements, see `Membrane.Element.Base`.
+  # Elements
+
+  Elements are units that produce, process or consume data. They can be linked
+  with `Membrane.Pipeline`, and thus form a pipeline able to perform complex data
+  processing. Each element defines a set of pads, through which it can be linked
+  with other elements. During playback, pads can either send (output pads) or
+  receive (input pads) data. For more information on pads, see
+  `Membrane.Element.Pad`.
+
+  To implement an element, one of base modules (`Membrane.Source`,
+  `Membrane.Filter`, `Membrane.Sink`)
+  has to be `use`d, depending on the element type:
+  - source, producing buffers (contain only output pads),
+  - filter, processing buffers (contain both input and output pads),
+  - sink, consuming buffers (contain only input pads).
+  For more information on each element type, check documentation for appropriate
+  base module.
+
+  ## Behaviours
+  Element-specific behaviours are specified in modules:
+  - `Membrane.Element.Base` - this module, behaviour common to all
+  elements,
+  - `Membrane.Element.WithOutputPads` - behaviour common to sources
+  and filters,
+  - `Membrane.Element.WithInputPads` - behaviour common to sinks and
+  filters,
+  - Base modules (`Membrane.Source`, `Membrane.Filter`,
+  `Membrane.Sink`) - behaviours specific to each element type.
+
+  ## Callbacks
+  Modules listed above provide specifications of callbacks that define elements
+  lifecycle. All of these callbacks have names with the `handle_` prefix.
+  They are used to define reaction to certain events that happen during runtime,
+  and indicate what actions framework should undertake as a result, besides
+  executing element-specific code.
+
+  For actions that can be returned by each callback, see `Membrane.Element.Action`
+  module.
   """
+
   alias Membrane.{Action, Core, Element, Event}
   alias Core.CallbackHandler
   alias Core.Element.OptionsSpecs
