06d5e2e4518d0e3ca0efd53d5c77ea1145b3e204
Dominik Stanaszek <dominik.stanaszek@swmansion.com>
2019-08-07 14:53:52 +0200

    Add docs to linking buffer

--- lib/membrane/bin/linking_buffer.ex
+++ lib/membrane/bin/linking_buffer.ex
@@ -1,12 +1,30 @@
 defmodule Membrane.Bin.LinkingBuffer do
   alias Membrane.Core.Message
   alias Membrane.Core.Element.PadModel
+  alias Membrane.Element.Pad
+  alias Membrane.Bin
   require Message
 
+  @type t :: Map.t()
+
+  @doc """
+  Creates a new linking buffer.
+  """
+  @spec new :: t()
   def new, do: Map.new()
 
-  # %{dest_pad => msg}
-  # TODO what with empty buf?
+  @doc """
+  This function sends a message to pad, IF AND ONLY IF
+  this pad is already linked. If it's not it is stored
+  and will be sent after calling `eval_for_pad()`.
+  Params:
+  * buf - buffer structure
+  * msg - message to be sent
+  * outgoing_pad - pad from which the message is supposed
+                   to be sent
+  * bin_state - state of the bin
+  """
+  @spec store_or_send(t(), Message.t(), Pad.ref_t(), Bin.State.t()) :: t()
   def store_or_send(buf, msg, outgoing_pad, bin_state) do
     case PadModel.get_data(bin_state, outgoing_pad) do
       {:ok, %{pid: dest_pid, other_ref: other_ref}} ->
@@ -18,6 +36,11 @@ defmodule Membrane.Bin.LinkingBuffer do
     end
   end
 
+  @doc """
+  Sends messages stored for a given outpud pad.
+  A link must already be available.
+  """
+  @spec eval_for_pad(t(), Pad.ref_t(), Bin.State.t()) :: t()
   def eval_for_pad(buf, pad, bin_state) do
     case Map.pop(buf, pad) do
       {nil, ^buf} ->
