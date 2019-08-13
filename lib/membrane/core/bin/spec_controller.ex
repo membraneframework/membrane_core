1861371fe70fe70e4bf055874a408691b66ce50b
Dominik Stanaszek <dominik.stanaszek@swmansion.com>
2019-08-13 14:48:47 +0200

    Check pid in guard

--- lib/membrane/core/bin/spec_controller.ex
+++ lib/membrane/core/bin/spec_controller.ex
@@ -120,17 +120,15 @@ defmodule Membrane.Core.Bin.SpecController do
     {{:ok, :cont}, state}
   end
 
-  defp handle_link(pid, args, state) do
-    case self() do
-      ^pid ->
-        with {{:ok, _spec} = res, state} <- apply(PadController, :handle_link, args ++ [state]),
-             {:ok, state} <- PadController.handle_linking_finished(state),
-             do: {res, state}
+  defp handle_link(pid, args, state) when pid == self() do
+    with {{:ok, _spec} = res, state} <- apply(PadController, :handle_link, args ++ [state]),
+         {:ok, state} <- PadController.handle_linking_finished(state),
+         do: {res, state}
+  end
 
-      _ ->
-        res = Message.call(pid, :handle_link, args)
-        {res, state}
-    end
+  defp handle_link(pid, args, state) do
+    Message.call(pid, :handle_link, args)
+    ~> {&1, state}
   end
 
   @impl true
