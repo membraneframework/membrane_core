324889d249034929b361cdbb415f2c3c7f855e68
Mateusz Front <mateusz.front@swmansion.com>
2019-08-19 17:37:44 +0200

    clock refactor

--- lib/membrane/clock.ex
+++ lib/membrane/clock.ex
@@ -3,14 +3,21 @@ defmodule Membrane.Clock do
   Clock is a Membrane utility that allows elements to measure time according to
   a particular clock, which can be e.g. a soundcard hardware clock.
 
-  Internally, Clock is a GenServer process that can be sent _updates_ (see `t:update_t`),
-  which are messages containing amount of time between current and subsequent update
-  according to some custom clock. Basing on updates, Clock calculates a _ratio_
-  between the Erlang clock and the custom clock, that is broadcasted to
-  _subscribers_ (see `subscribe/1`)- processes willing to synchronize to the custom clock. Subscribers
-  can adjust their timers according to received ratio - timers started with `:timer`
-  action in elements do it automatically. Initial ratio is equal to 1, which means
-  that if no updates are received, clock is synchronized to the Erlang clock.
+  Internally, Clock is a GenServer process that can receive _updates_ (see `t:update_t`),
+  which are messages containing amount of time until the next update.
+  For example, a sink playing audio to the sound card can send an update before
+  each write to the soundcard buffer (for practical reasons that can be done every
+  100 or 1000 writes). Although it might be more intuitive to send updates with
+  the time passed, in practice the described approach turns out to be more convenient,
+  as it simplifies the first update.
+
+  Basing on updates, Clock calculates a _ratio_ between the Erlang clock and
+  the custom clock, that is broadcasted (see t:ratio_t) to _subscribers_
+  (see `subscribe/2`) - processes willing to synchronize to the custom clock.
+  Subscribers can adjust their timers according to received ratio - timers started
+  with `:timer` action in elements do it automatically. Initial ratio is equal
+  to 1, which means that if no updates are received, Clock is synchronized to the
+  Erlang clock.
 
   ## Proxy mode
   Clock can work in _proxy_ mode, which means it cannot receive updates, but
@@ -20,36 +27,64 @@ defmodule Membrane.Clock do
   """
   use Bunch
   use GenServer
-  require Membrane.Core.Message
   alias Membrane.Time
   alias Membrane.Core.Message
 
+  @typedoc """
+  Update message received by the Clock. It should contain the time till the next
+  update.
+  """
   @type update_t ::
           {:membrane_clock_update,
-           (milliseconds :: non_neg_integer | Ratio.t())
-           | {milliseconds :: non_neg_integer, denominator :: pos_integer}}
+           milliseconds ::
+             non_neg_integer
+             | Ratio.t()
+             | {nominator :: non_neg_integer, denominator :: pos_integer}}
 
+  @typedoc """
+  Ratio message sent by the Clock to all its subscribers. It contains the ratio
+  of a custom clock to the Erlang clock.
+  """
+  @type ratio_t :: {:membrane_clock_ratio, clock :: pid, Ratio.t()}
+
+  @type option_t :: {:proxy, boolean} | {:proxy_for, pid}
+
+  @spec start_link([option_t], GenServer.options()) :: GenServer.on_start()
   def start_link(options \\ [], gen_server_options \\ []) do
     GenServer.start_link(__MODULE__, options, gen_server_options)
   end
 
+  @spec start_link!([option_t], GenServer.options()) :: pid | no_return
   def start_link!(options \\ [], gen_server_options \\ []) do
     {:ok, clock} = start_link(options, gen_server_options)
     clock
   end
 
+  @doc """
+  Subscribes `pid` for receiving `t:ratio_t` messages from the clock.
+
+  This function can be called multiple times from the same process. To unsubscribe,
+  `unsubscribe/2` should be called the same amount of times.
+  """
+  @spec subscribe(clock :: pid, subscriber :: pid) :: :ok
   def subscribe(clock, pid \\ self()) do
-    Message.send(clock, :clock_subscribe, pid)
-    :ok
+    GenServer.cast(clock, {:clock_subscribe, pid})
   end
 
+  @doc """
+  Unsubscribes `pid` from receiving `t:ratio_t` messages from the clock.
+
+  For unsubscription to take effect, `unsubscribe/2` should be called the same
+  amount of times as `subscribe/2`.
+  """
+  @spec unsubscribe(clock :: pid, subscriber :: pid) :: :ok
   def unsubscribe(clock, pid \\ self()) do
-    Message.send(clock, :clock_unsubscribe, pid)
+    GenServer.cast(clock, {:clock_unsubscribe, pid})
   end
 
+  @spec proxy_for(clock :: pid, clock_to_proxy_for :: pid) :: :ok
   def proxy_for(clock, clock_to_proxy_for) do
-    Message.send(clock, :proxy_for, clock_to_proxy_for)
-    :ok
+    GenServer.cast(clock, {:proxy_for, clock_to_proxy_for})
   end
 
   @impl GenServer
@@ -74,40 +109,28 @@ defmodule Membrane.Clock do
   end
 
   @impl GenServer
-  def handle_info({:membrane_clock_update, till_next}, %{proxy: false} = state) do
-    {:noreply, handle_clock_update(till_next, state)}
-  end
-
-  @impl GenServer
-  def handle_info({:membrane_clock_ratio, _pid, ratio}, %{proxy: true} = state) do
-    {:noreply, send_and_update_ratio(ratio, state)}
-  end
-
-  @impl GenServer
-  def handle_info(Message.new(:proxy_for, proxy_for), %{proxy: true} = state) do
-    if state.proxy_for do
-      Message.send(state.proxy_for, :clock_unsubscribe, self())
-    end
+  def handle_cast({:proxy_for, proxy_for}, %{proxy: true} = state) do
+    if state.proxy_for, do: unsubscribe(state.proxy_for)
 
     state = %{state | proxy_for: proxy_for}
 
     state =
       if proxy_for do
-        Message.send(proxy_for, :clock_subscribe, self())
+        subscribe(proxy_for)
         state
       else
-        send_and_update_ratio(1, state)
+        broadcast_and_update_ratio(1, state)
       end
 
     {:noreply, state}
   end
 
   @impl GenServer
-  def handle_info(Message.new(:clock_subscribe, pid), state) do
+  def handle_cast({:clock_subscribe, pid}, state) do
     state
     |> update_in([:subscribers, pid], fn
       nil ->
-        send(pid, {:membrane_clock_ratio, self(), state.ratio})
+        send_ratio(pid, state.ratio)
         monitor = Process.monitor(pid)
         %{monitor: monitor, subscriptions: 1}
 
@@ -118,13 +141,31 @@ defmodule Membrane.Clock do
   end
 
   @impl GenServer
-  def handle_info(Message.new(:clock_unsubscribe, pid), state) do
-    Process.demonitor(state.subscribers[pid].monitor)
+  def handle_cast({:clock_unsubscribe, pid}, state) do
+    if Map.has_key?(state.subscribers, pid) do
+      {subs, state} =
+        state |> Bunch.Access.get_updated_in([:subscribers, pid, :subscriptions], &(&1 - 1))
 
-    {subs, state} =
-      state |> Bunch.Access.get_updated_in([:subscribers, pid, :subscriptions], &(&1 - 1))
+      if subs == 0, do: handle_unsubscribe(pid, state), else: state
+    else
+      state
+    end
+    ~> {:noreply, &1}
+  end
 
-    state = if subs == 0, do: handle_unsubscribe(pid, state), else: state
+  @impl GenServer
+  def handle_info({:membrane_clock_update, till_next}, %{proxy: false} = state) do
+    {:noreply, handle_clock_update(till_next, state)}
+  end
+
+  @impl GenServer
+  def handle_info({:membrane_clock_ratio, pid, ratio}, %{proxy: true, proxy_for: pid} = state) do
+    {:noreply, broadcast_and_update_ratio(ratio, state)}
+  end
+
+  @impl GenServer
+  # When ratio from previously proxied clock incomes after unsubscribing
+  def handle_info({:membrane_clock_ratio, _pid, _ratio}, %{proxy: true} = state) do
     {:noreply, state}
   end
 
@@ -134,6 +175,7 @@ defmodule Membrane.Clock do
   end
 
   defp handle_unsubscribe(pid, state) do
+    Process.demonitor(state.subscribers[pid].monitor)
     state |> Bunch.Access.delete_in([:subscribers, pid])
   end
 
@@ -156,11 +198,16 @@ defmodule Membrane.Clock do
     clock_time = clock_time + from_previous * Time.millisecond(1)
     ratio = clock_time / (Time.monotonic_time() - state.init_time)
     state = %{state | clock_time: clock_time, till_next: till_next}
-    send_and_update_ratio(ratio, state)
+    broadcast_and_update_ratio(ratio, state)
   end
 
-  defp send_and_update_ratio(ratio, state) do
-    state.subscribers |> Bunch.KVList.each_key(&send(&1, {:membrane_clock_ratio, self(), ratio}))
+  defp broadcast_and_update_ratio(ratio, state) do
+    state.subscribers |> Bunch.KVList.each_key(&send_ratio(&1, ratio))
     %{state | ratio: ratio}
+  end
+
+  defp send_ratio(pid, ratio) do
+    send(pid, {:membrane_clock_ratio, self(), ratio})
+    :ok
   end
 end
