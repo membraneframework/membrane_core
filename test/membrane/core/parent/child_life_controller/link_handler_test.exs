066bcc52ff0eead60ce1125ff01e853965cd3a7b
Mateusz Front <mateusz.front@swmansion.com>
2019-11-04 14:57:32 +0100

    parent link handler tests

---
+++ test/membrane/core/parent/child_life_controller/link_handler_test.exs
@@ -0,0 +1,157 @@
+defmodule Membrane.Core.Parent.ChildLifeController.LinkHandlerTest do
+  use ExUnit.Case
+
+  alias Membrane.Core.Parent.ChildLifeController.LinkHandler
+  alias Membrane.Core.Parent.{ChildEntry, Link}
+  alias Membrane.LinkError
+  alias Membrane.Support.Bin.TestBins.{TestFilter, TestDynamicPadFilter}
+
+  defp get_state(child_module, availability \\ :always) do
+    %Membrane.Core.Bin.State{
+      module: nil,
+      name: :my_bin,
+      clock_proxy: nil,
+      children: [a: %ChildEntry{module: child_module}, b: %ChildEntry{module: child_module}],
+      pads: %{
+        info:
+          [input: %{availability: availability}, output: %{availability: availability}]
+          |> Enum.flat_map(fn {name, info} -> [{name, info}, {{:private, name}, info}] end)
+          |> Map.new(),
+        data: %{}
+      }
+    }
+  end
+
+  defp endpoints(links) do
+    links |> Enum.flat_map(&[&1.from, &1.to])
+  end
+
+  describe "resolve links" do
+    import Membrane.ParentSpec
+    require Membrane.Pad
+    alias Membrane.Pad
+
+    test "should work for static pads" do
+      {:ok, links} = Link.from_spec([link(:a) |> to(:b)])
+      resolved_links = LinkHandler.resolve_links(links, get_state(TestFilter))
+      endpoints(resolved_links) |> Enum.each(&assert &1.pad_ref == &1.pad)
+    end
+
+    test "should work for dynamic pads" do
+      {:ok, links} = Link.from_spec([link(:a) |> to(:b)])
+
+      resolved_links = LinkHandler.resolve_links(links, get_state(TestDynamicPadFilter))
+
+      endpoints(resolved_links)
+      |> Enum.each(fn %{pad: pad, pad_ref: pad_ref} ->
+        assert Pad.ref(^pad, _) = pad_ref
+      end)
+
+      {:ok, links} =
+        Link.from_spec([
+          link(:a) |> via_out(Pad.ref(:output, :x)) |> via_in(Pad.ref(:input, :y)) |> to(:b)
+        ])
+
+      resolved_links = LinkHandler.resolve_links(links, get_state(TestDynamicPadFilter))
+
+      assert [%{pad_ref: Pad.ref(:output, :x)}, %{pad_ref: Pad.ref(:input, :y)}] =
+               resolved_links |> endpoints() |> Enum.sort()
+    end
+
+    test "should work for bin static pads" do
+      {:ok, links} = Link.from_spec([link_bin_input() |> to(:a) |> to_bin_output()])
+
+      resolved_links = LinkHandler.resolve_links(links, get_state(TestFilter))
+      endpoints(resolved_links) |> Enum.each(&assert &1.pad_ref == &1.pad)
+
+      endpoints(resolved_links)
+      |> Enum.filter(&(&1.child == {Membrane.Bin, :itself}))
+      |> Enum.each(&assert {:private, _} = &1.pad_ref)
+    end
+
+    test "should work for bin dynamic pads" do
+      {:ok, links} =
+        Link.from_spec([
+          link_bin_input(Pad.ref(:input, :x)) |> to(:a) |> to_bin_output(Pad.ref(:output, :y))
+        ])
+
+      state =
+        get_state(TestFilter, :on_request)
+        |> put_in([:pads, :data], %{
+          Pad.ref(:input, :x) => %Pad.Data{},
+          Pad.ref(:output, :y) => %Pad.Data{}
+        })
+
+      resolved_links = LinkHandler.resolve_links(links, state)
+      endpoints(resolved_links) |> Enum.each(&assert &1.pad_ref == &1.pad)
+
+      endpoints(resolved_links)
+      |> Enum.filter(&(&1.child == {Membrane.Bin, :itself}))
+      |> Enum.each(&assert Pad.ref({:private, _}, _) = &1.pad_ref)
+    end
+
+    test "should fail when trying to link non-existent child" do
+      {:ok, links} = Link.from_spec([link(:a) |> to(:c)])
+
+      assert_raise LinkError, ~r/child :c does not exist/i, fn ->
+        LinkHandler.resolve_links(links, get_state(TestFilter))
+      end
+    end
+
+    test "should fail when trying to link non-existent pad" do
+      {:ok, links} = Link.from_spec([link(:a) |> via_out(:x) |> to(:b)])
+
+      assert_raise LinkError, ~r/child :a does not have pad :x/i, fn ->
+        LinkHandler.resolve_links(links, get_state(TestFilter))
+      end
+    end
+
+    test "should fail when trying to pass dynamic pad ref to a static pad" do
+      {:ok, links} = Link.from_spec([link(:a) |> via_out(Pad.ref(:output, :x)) |> to(:b)])
+
+      assert_raise LinkError,
+                   ~r/dynamic pad ref .*membrane.pad.*:output.* passed for static pad of child :a/i,
+                   fn ->
+                     LinkHandler.resolve_links(links, get_state(TestFilter))
+                   end
+    end
+
+    test "should fail when trying to link non-existent bin pad" do
+      {:ok, links} = Link.from_spec([link_bin_input(:x) |> to(:b)])
+
+      assert_raise LinkError, ~r/bin :my_bin does not have pad :x/i, fn ->
+        LinkHandler.resolve_links(links, get_state(TestFilter))
+      end
+    end
+
+    test "should fail when trying to link dynamic bin pad and passed only name" do
+      {:ok, links} = Link.from_spec([link_bin_input(:input) |> to(:b)])
+
+      assert_raise LinkError,
+                   ~r/exact reference not passed when linking dynamic bin pad :input/i,
+                   fn ->
+                     LinkHandler.resolve_links(links, get_state(TestFilter, :on_request))
+                   end
+    end
+
+    test "should fail when trying to link dynamic bin pad that is not externally linked yet" do
+      {:ok, links} = Link.from_spec([link_bin_input(Pad.ref(:input, :x)) |> to(:b)])
+
+      assert_raise LinkError,
+                   ~r/linking dynamic bin pad .*membrane.pad.*:input.* not .* externally linked/i,
+                   fn ->
+                     LinkHandler.resolve_links(links, get_state(TestFilter, :on_request))
+                   end
+    end
+
+    test "should fail when trying to pass dynamic pad ref to a static bin pad" do
+      {:ok, links} = Link.from_spec([link(:a) |> to_bin_output(Pad.ref(:output, :x))])
+
+      assert_raise LinkError,
+                   ~r/dynamic pad ref .*membrane.pad.*:output.* passed for static pad of bin :my_bin/i,
+                   fn ->
+                     LinkHandler.resolve_links(links, get_state(TestFilter))
+                   end
+    end
+  end
+end
