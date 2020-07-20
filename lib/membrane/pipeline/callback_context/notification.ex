d0bd66822df24f4c1f80f1a31eb4ea8543570b1c
Mateusz Front <mateusz.front@swmansion.com>
2020-07-20 15:42:34 +0200

    Update lib/membrane/pipeline/callback_context/notification.ex
    Co-authored-by: Bartosz Błaszków <bartosz.blaszkow@swmansion.com>

--- lib/membrane/pipeline/callback_context/notification.ex
+++ lib/membrane/pipeline/callback_context/notification.ex
@@ -1,7 +1,7 @@
 defmodule Membrane.Pipeline.CallbackContext.Notification do
   @moduledoc """
   Structure representing a context that is passed to the callback when
-  pipeline receives notification.
+  pipeline receives a notification.
   """
   use Membrane.Core.Pipeline.CallbackContext
 end
