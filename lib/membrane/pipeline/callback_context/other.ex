9a3b586fdb36837176962542cc3beb39be49d177
Mateusz Front <mateusz.front@swmansion.com>
2020-07-20 15:42:16 +0200

    Update lib/membrane/pipeline/callback_context/other.ex
    Co-authored-by: Bartosz Błaszków <bartosz.blaszkow@swmansion.com>

--- lib/membrane/pipeline/callback_context/other.ex
+++ lib/membrane/pipeline/callback_context/other.ex
@@ -1,7 +1,7 @@
 defmodule Membrane.Pipeline.CallbackContext.Other do
   @moduledoc """
   Structure representing a context that is passed to the callback when
-  pipeline receives unrecognized message.
+  pipeline receives an unrecognized message.
   """
   use Membrane.Core.Pipeline.CallbackContext
 end
