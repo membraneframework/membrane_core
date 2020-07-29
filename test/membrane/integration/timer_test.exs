fe79b2a6fd4ef78d98f52a524fb901a4572e8f74
Mateusz Front <mateusz.front@swmansion.com>
2020-07-29 14:15:12 +0200

    fix stop_timer from handle_tick

---
+++ test/membrane/integration/timer_test.exs
@@ -0,0 +1,34 @@
+defmodule Membrane.Integration.TimerTest do
+  use ExUnit.Case, async: true
+  import Membrane.Testing.Assertions
+  alias Membrane.{Pipeline, Testing, Time}
+
+  defmodule Element do
+    use Membrane.Source
+
+    def_output_pad :output, mode: :push, caps: :any
+
+    @impl true
+    def handle_prepared_to_playing(_ctx, state) do
+      {{:ok, start_timer: {:timer, Time.milliseconds(100)}}, state}
+    end
+
+    @impl true
+    def handle_tick(:timer, _ctx, state) do
+      {{:ok, notify: :tick, stop_timer: :timer}, state}
+    end
+  end
+
+  test "timer" do
+    {:ok, pipeline} =
+      Testing.Pipeline.start_link(%Testing.Pipeline.Options{
+        elements: [element: Element, sink: Testing.Sink]
+      })
+
+    Pipeline.play(pipeline)
+    assert_pipeline_playback_changed(pipeline, _, :playing)
+    assert_pipeline_notified(pipeline, :element, :tick)
+    Pipeline.stop(pipeline)
+    assert_pipeline_playback_changed(pipeline, _, :stopped)
+  end
+end
