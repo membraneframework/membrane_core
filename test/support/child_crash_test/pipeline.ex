41b3f423731f80b663dbd67bc32814a592a1229e
AndrzejPodo <andrzejpodobinski2@gmail.com>
2021-04-19 12:58:37 +0200

    improve code sanity in ChildCrashTest.Pipeline

--- test/support/child_crash_test/pipeline.ex
+++ test/support/child_crash_test/pipeline.ex
@@ -75,15 +75,13 @@ defmodule Membrane.Support.ChildCrashTest.Pipeline do
     children = [{source_name, Testing.Source}] ++ (filters_names |> Enum.map(&{&1, Filter}))
     children_names = children |> Enum.map(&elem(&1, 0))
 
-    children_count = length(children)
-
     links =
-      children_names
-      |> Enum.with_index()
-      |> Enum.map(fn {child, i} ->
-        if i < children_count - 1,
-          do: link(child) |> to(Enum.at(children_names, i + 1)),
-          else: link(child) |> to(:center_filter)
+      Enum.chunk_every(children_names, 2, 1)
+      |> Enum.map(fn pair ->
+        case pair do
+          [first_elem, second_elem] -> link(first_elem) |> to(second_elem)
+          [elem] -> link(elem) |> to(:center_filter)
+        end
       end)
 
     spec = %Membrane.ParentSpec{
