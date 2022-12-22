4d72882b01726c0f43ae1c1e52cfcfabd2658821
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2022-12-22 16:04:15 +0100

    Extend Membrane.Playback moduledoc

--- lib/membrane/playback.ex
+++ lib/membrane/playback.ex
@@ -1,6 +1,14 @@
 defmodule Membrane.Playback do
   @moduledoc """
   Playback defines whether media is processed within a pipeline (`:playing`) or not (`:stopped`).
+
+  Component playback will not enter `:playing` state until its parent playback is playing and
+  all components from the `spec`, that started this component have ended their setups and linked
+  their pads.
+
+  By default, component setup ends with the end of `handle_setup/2` callback.
+  If `{:setup, :incomplete}` is returned there, setup lasts until `{:setup, :complete}`
+  is returned from antoher callback.
   """
   @type t :: :stopped | :playing
 end
