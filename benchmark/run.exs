4a91605895556c7c4ad580ca3010c53092d6c88e
varsill <lukasz.kita0@gmail.com>
2023-02-10 09:57:02 +0100

    update tests grid

--- benchmark/run.exs
+++ benchmark/run.exs
@@ -37,7 +37,7 @@ defmodule Benchmark.Run do
       max_random: 1,
       number_of_filters: 10,
       number_of_buffers: 50000,
-      buffer_size: 100_000
+      buffer_size: 1
     ],
     [
       reductions: 10_000,
