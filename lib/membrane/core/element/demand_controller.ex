e713e528e1e19d26061694045f170d60315b7ed6
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2023-05-18 14:29:04 +0200

    Refactor getting demand unit in demand controller

--- lib/membrane/core/element/demand_controller.ex
+++ lib/membrane/core/element/demand_controller.ex
@@ -90,7 +90,12 @@ defmodule Membrane.Core.Element.DemandController do
   def decrease_demand_by_outgoing_buffers(pad_ref, buffers, state) do
     pad_data = PadModel.get_data!(state, pad_ref)
 
-    demand_unit = pad_data.demand_unit || pad_data.other_demand_unit || :buffers
+    demand_unit =
+      case pad_data.flow_control do
+        :push -> pad_data.other_demand_unit || :buffers
+        _pull_or_auto -> pad_data.demand_unit
+      end
+
     buffers_size = Buffer.Metric.from_unit(demand_unit).buffers_size(buffers)
 
     demand_snapshot = pad_data.demand_snapshot - buffers_size
