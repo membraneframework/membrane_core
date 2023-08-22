4f471c2e4dd5b388474ef6366a7a949404021000
Feliks Pobiedziński <38541925+FelonEkonom@users.noreply.github.com>
2023-08-22 18:02:27 +0200

    Send end of stream, even if there was no start of stream before (#577)

--- lib/membrane/pipeline/callback_context.ex
+++ lib/membrane/pipeline/callback_context.ex
@@ -8,6 +8,9 @@ defmodule Membrane.Pipeline.CallbackContext do
 
   Field `:from` is present only in `c:Membrane.Pipeline.handle_call/3`.
 
+  Field `:start_of_stream_received?` is present only in
+  `c:Membrane.Pipeline.handle_element_end_of_stream/4`.
+
   Fields `:members` and `:crash_initiator` are present only in
   `c:Membrane.Pipeline.handle_crash_group_down/3`.
   """
@@ -19,6 +22,7 @@ defmodule Membrane.Pipeline.CallbackContext do
           :utility_supervisor => Membrane.UtilitySupervisor.t(),
           optional(:from) => [GenServer.from()],
           optional(:members) => [Membrane.Child.name()],
-          optional(:crash_initiator) => Membrane.Child.name()
+          optional(:crash_initiator) => Membrane.Child.name(),
+          optional(:start_of_stream_received?) => boolean()
         }
 end
