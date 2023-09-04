7c5263129e46683fcaa94311b6190fc02f2c8e20
Feliks Pobiedziński <38541925+FelonEkonom@users.noreply.github.com>
2023-09-04 14:05:28 +0200

    Validate Testing.Pipeline options (#593)

--- lib/membrane/testing/pipeline.ex
+++ lib/membrane/testing/pipeline.ex
@@ -145,6 +145,8 @@ defmodule Membrane.Testing.Pipeline do
   end
 
   defp do_start(type, options) do
+    :ok = validate_options!(options)
+
     {process_options, options} = Keyword.split(options, [:name])
     options = Keyword.put_new(options, :test_process, self())
     apply(Pipeline, type, [__MODULE__, options, process_options])
@@ -527,5 +529,20 @@ defmodule Membrane.Testing.Pipeline do
 
   defp combine_results({custom_actions, custom_state}, {actions, state}) do
     {Enum.concat(custom_actions, actions), Map.put(state, :custom_pipeline_state, custom_state)}
+  end
+
+  defp validate_options!(options) do
+    allowed_keys_1 = [:module, :mode, :spec, :test_process, :name, :raise_on_child_pad_removed?]
+    allowed_keys_2 = [:module, :custom_args, :test_process, :name]
+
+    with {:error, _keys} <- Keyword.validate(options, allowed_keys_1),
+         {:error, _keys} <- Keyword.validate(options, allowed_keys_2) do
+      raise """
+      Options passed to #{inspect(__MODULE__)} start function has to fulfill type
+      #{inspect(__MODULE__)}.options, while they are #{inspect(options)}
+      """
+    else
+      {:ok, _keyword} -> :ok
+    end
   end
 end
