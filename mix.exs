d0680e9c66d51ade868cd6a93329596a4ecf0d6a
Feliks Pobiedziński <38541925+FelonEkonom@users.noreply.github.com>
2023-10-18 13:03:01 +0200

    Write upgrading guide for v1.0.0 (#646)
    * Write upgrading guide for v1.0.0
    
    * Update guides/upgrading/v1.0.0.md
    
    Co-authored-by: Mateusz Front <mateusz.front@swmansion.com>
    
    * Implement suggestion from CR
    
    ---------
    
    Co-authored-by: Mateusz Front <mateusz.front@swmansion.com>

--- mix.exs
+++ mix.exs
@@ -66,6 +66,7 @@ defmodule Membrane.Mixfile do
         "guides/upgrading/v0.11.md",
         "guides/upgrading/v1.0.0-rc0.md",
         "guides/upgrading/v1.0.0-rc1.md",
+        "guides/upgrading/v1.0.0.md",
         LICENSE: [title: "License"]
       ],
       formatters: ["html"],
