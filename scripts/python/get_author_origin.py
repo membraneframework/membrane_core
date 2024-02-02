a0e0d94ca4ef04445f87023971a0c326c5092958
Bartek Chaliński <b.chalinski@gmail.com>
2024-02-02 17:10:01 +0100

    Python scripts enhancement (#740)
    * add try catch
    
    * more debug prints

--- scripts/python/get_author_origin.py
+++ scripts/python/get_author_origin.py
@@ -3,10 +3,14 @@ import sys, json;
 membrane_team = json.load(sys.stdin)
 pr_author = sys.argv[1]
 
-for person in membrane_team:
-  if person["login"] == pr_author:
-    print("MEMBRANE")
-    sys.exit(0)
+try:
+  for person in membrane_team:
+    if person["login"] == pr_author:
+      print("MEMBRANE")
+      sys.exit(0)
 
-print("COMMUNITY")
-
+  print("COMMUNITY")
+except:
+    print("An exception occurred, provided JSON:")
+    print(membrane_team)
+    print("provided PR_AUTHOR:", pr_author)
