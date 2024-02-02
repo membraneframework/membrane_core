79fa9080ff543ff82d140ac901cc6a36abe35e77
Bartek Chaliński <b.chalinski@gmail.com>
2024-02-02 16:51:27 +0100

    add try catch

--- scripts/python/get_ticket_id.py
+++ scripts/python/get_ticket_id.py
@@ -1,6 +1,12 @@
 import sys, json;
 
-project_items = json.load(sys.stdin)["items"]
-pr_url = sys.argv[1]
-[id] = [item["id"] for item in project_items if ("url" in item["content"] and item["content"]["url"] == pr_url)]
-print(id)
+full_json = json.load(sys.stdin)
+
+try:
+    project_items = full_json["items"]
+    pr_url = sys.argv[1]
+    [id] = [item["id"] for item in project_items if ("url" in item["content"] and item["content"]["url"] == pr_url)]
+    print(id)
+except:
+    print("An exception occurred, provided JSON:")
+    print(full_json)
