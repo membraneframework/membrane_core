3ea52d2c4deb0b68169ddcbf2fb8aa200a5c352d
Bartek Chaliński <b.chalinski@gmail.com>
2024-02-02 17:46:55 +0100

    check if pr author is membrane admin (#741)

--- scripts/python/get_author_origin.py
+++ scripts/python/get_author_origin.py
@@ -3,6 +3,10 @@ import sys, json;
 membrane_team = json.load(sys.stdin)
 pr_author = sys.argv[1]
 
+if pr_author == "membraneframeworkadmin":
+  print("MEMBRANE")
+  sys.exit(0)
+
 try:
   for person in membrane_team:
     if person["login"] == pr_author:
