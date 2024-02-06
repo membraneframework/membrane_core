ca41d42971bb71f4dcfc93c8eb351e9faf0e10b8
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2024-02-06 17:18:31 +0100

    Fix bug

--- lib/membrane/core/element/event_controller.ex
+++ lib/membrane/core/element/event_controller.ex
@@ -40,9 +40,10 @@ defmodule Membrane.Core.Element.EventController do
       Telemetry.report_metric(:event, 1, inspect(pad_ref))
 
       state =
-        if event in [%Events.StartOfStream{}, %Events.EndOfStream{}] or data.start_of_stream?,
-          do: state,
-          else: handle_start_of_stream(pad_ref, state)
+        if event in [%Events.StartOfStream{}, %Events.EndOfStream{}] or data.start_of_stream? or
+             data.direction == :output,
+           do: state,
+           else: handle_start_of_stream(pad_ref, state)
 
       if not Event.async?(event) and buffers_before_event_present?(data) do
         PadModel.update_data!(
