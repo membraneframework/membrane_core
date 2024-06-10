8fad39cf7c85038b9c04f066ce04a04692224241
Feliks Pobiedziński <38541925+FelonEkonom@users.noreply.github.com>
2024-06-10 17:09:36 +0200

    Update lib/membrane/core/macros.ex
    Co-authored-by: Łukasz Kita <lukasz.kita0@gmail.com>

--- lib/membrane/core/macros.ex
+++ lib/membrane/core/macros.ex
@@ -2,7 +2,7 @@ defmodule Membrane.Core.Macros do
   @moduledoc false
 
   # For some reason GenServer processes sometimes don't print logs about crash, so
-  # we add this macro, to ensure that error logs wil be always logged
+  # we add this macro, to ensure that error logs are always printed
   defmacro log_on_error(do: code) do
     quote do
       try do
