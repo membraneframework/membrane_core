757b31f9f64f27f55851d15ec82c8956671858cd
noarkhh <jakub.pryc@gmail.com>
2026-03-19 13:55:59 +0100

    Explain difference between Endpoint and Filter

--- lib/membrane/endpoint.ex
+++ lib/membrane/endpoint.ex
@@ -8,14 +8,24 @@ defmodule Membrane.Endpoint do
   and `Membrane.Element.WithInputPads`.
 
   Endpoint can have both input and output pads. Job of usual endpoint is both, to
-  receive some data on such pad and consume it (write to a soundcard, send through
+  receive some data on input pads and consume it (write to a soundcard, send through
   TCP etc.) and to produce some data (read from soundcard, download through HTTP,
-  etc.) and send it through such pad. If the pad has the flow control set to
+  etc.) and send on output pads. If the pad has the flow control set to
   `:manual`, then endpoint is also responsible for receiving demands on the output
   pad and requesting them on the input pad (for more details, see
   `c:Membrane.Element.WithOutputPads.handle_demand/5` callback).
   Endpoints, like all elements, can of course have multiple pads if needed to
   provide more complex solutions.
+
+  Although Endpoints may seem similair to [Filters](`m:Membrane.Filter`), there are 
+  some important differences. While Filters can be thought as parts of a Pipeline that 
+  take in data, process it in some way, and then pass it along, Endpoints create 
+  "holes" in the pipeline. They behave more like a [Sink](`m:Membrane.Sink`) and a 
+  [Source](`m:Membrane.Source`) in a single element - media they consume and produce are
+  parts of different streams. The main consequence of this is the fact that 
+  they separate the flow control of the pipeline ahead of them and behind them, 
+  and in turn their input pads behave like ones of a Sink, and their output pads behave
+  like ones of a Source.
   """
 
   alias Membrane.Core.DocsHelper
